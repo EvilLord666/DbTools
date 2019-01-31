272fb3e924524174880bd631c115304e5916c9d5
Ushakov Michael <um.nix.user@gmail.com>
2019-01-31 14:23:16 +0300

    extensions + tests

---
+++ DbTools/DbTools.Simple.Tests/TestScripts/TestServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+using DbTools.Core;
+using DbTools.Core.Managers;
+using DbTools.Simple.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace DbTools.Simple.Tests.TestScripts
+{
+    public class TestServiceCollectionExtensions
+    {
+        public TestServiceCollectionExtensions()
+        {
+            _services.AddLogging();
+            _services.AddDbManager(DbEngine.SqlServer);
+        }
+
+        [Fact]
+        public void TestAddDbManager()
+        {
+            ServiceProvider serviceProvider = _services.BuildServiceProvider();
+            IDbManager dbManager = serviceProvider.GetService<IDbManager>();
+            Assert.NotNull(dbManager);
+        }
+        
+        private readonly ServiceCollection _services = new ServiceCollection();
+    }
+}
