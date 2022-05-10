db44de9d02ae99f2a39367410477bc81406d7273
Ushakov Michael <um.nix.user@gmail.com>
2022-05-10 20:38:42 +0500

    intermediate

--- DbTools/DbTools.Simple/Factories/DbParameterFactory.cs
+++ DbTools/DbTools.Simple/Factories/DbParameterFactory.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
 using DbTools.Core;
@@ -11,14 +12,19 @@ namespace DbTools.Simple.Factories
 {
     public static class DbParameterFactory
     {
-        public static DbParameter Create(DbEngine dbEngine, string parameterName, int parameterType, 
+        public static DbParameter Create(DbEngine dbEngine, string parameterName, int parameterType, object value,
                                          ParameterDirection parameterDirection = ParameterDirection.Input)
         {
             if (dbEngine == DbEngine.SqlServer)
-                return new SqlParameter(parameterName, (SqlDbType)parameterType)
+            {
+                SqlDbType type = (SqlDbType)parameterType;
+                return new SqlParameter(parameterName, type)
                 {
-                    Direction = parameterDirection
+                    Direction = parameterDirection,
+                    Value = MapSqlParameterValue(type, value)
                 };
+            }
+
             if (dbEngine == DbEngine.MySql)
                 return new MySqlParameter(parameterName, (MySqlDbType)parameterType)
                 {
@@ -31,5 +37,31 @@ namespace DbTools.Simple.Factories
                 };
             throw new NotImplementedException("Other db engine are not supported yet, please add a github issue https://github.com/EvilLord666/DbTools");
         }
+
+        private static object MapSqlParameterValue(SqlDbType type, object value)
+        {
+            try
+            {
+                if (!SqlServerTypesMapping.ContainsKey(type))
+                    return value;
+                return Convert.ChangeType(value, SqlServerTypesMapping[type]);
+            }
+            catch (Exception e)
+            {
+                return value;
+            }
+        }
+
+        private static readonly IDictionary<SqlDbType, Type> SqlServerTypesMapping = new Dictionary<SqlDbType, Type>()
+        {
+            { SqlDbType.BigInt, typeof(long)},
+            { SqlDbType.Binary, typeof(byte[])},
+            { SqlDbType.Bit, typeof(bool)},
+            { SqlDbType.Char, typeof(string)},
+            { SqlDbType.DateTime, typeof(DateTime)},
+            { SqlDbType.DateTimeOffset, typeof(DateTimeOffset)},
+            { SqlDbType.Decimal, typeof(decimal)},
+            { SqlDbType.Float, typeof(DateTime)}
+        };
     }
 }
